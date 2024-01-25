f2f725ec8d55223f3bd5e773f2a00f5dfa73da00
cleciopinheiro <cleciosantosdeveloper@gmail>
2024-01-25 00:02:59 -0300

    Update

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,13 +1,4 @@
 import React from "react";
-import {
-  RxDiscordLogo,
-  RxGithubLogo,
-  RxInstagramLogo,
-  RxTwitterLogo,
-  RxLinkedinLogo,
-} from "react-icons/rx";
-
-import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
     const date = new Date();
