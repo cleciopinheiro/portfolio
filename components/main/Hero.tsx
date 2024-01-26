ed27806a31446c1d73d5eae9c9610379f8a5fde2
cleciopinheiro <clecio.pinheiro@outlook.com>
2024-01-25 21:43:15 -0300

    fix: Applying more extensions to videos

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -11,6 +11,7 @@ const Hero = () => {
         className="rotate-180 absolute top-[-340px] md:top-[-220px] sm:top-[-420px] h-full w-full left-0 z-[1] object-cover sm:object-contain sm:scale-[2.5]"
       >
         <source src="/blackhole.webm" type="video/webm" />
+        <source src="/blackhole.h264" type="video/h264" />
       </video>
       <HeroContent />
     </div>
