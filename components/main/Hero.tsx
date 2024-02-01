65b97a326a78df7298062e69cbe952d661b8b564
cleciopinheiro <cleciosantosdeveloper@gmail>
2024-01-31 21:56:00 -0300

    fix: Changed the h264 format to mp4 of the blackhole video

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -8,10 +8,11 @@ const Hero = () => {
         autoPlay
         muted
         loop
+        playsInline
         className="rotate-180 absolute top-[-340px] md:top-[-220px] sm:top-[-490px] h-full w-full left-0 z-[1] object-cover sm:-scale-100 sm:rotate-0"
       >
         <source src="/blackhole.webm" type="video/webm" />
-        <source src="/blackhole.h264" type="video/h264" />
+        <source src="/blackhole.mp4" type="video/mp4" />
       </video>
       <HeroContent />
     </div>
